9b866ee6232052b7d8e8ca5cd2423e4bf18231c0
Andrew Richmond <41588533+acrmine@users.noreply.github.com>
2025-11-21 22:20:02 -0800

    added kenny textures and got some 3d placement going

--- main.lua
+++ main.lua
@@ -1,27 +1,31 @@
--- written by groverbuger for g3d
--- may 2021
--- MIT license
-
+-- Imports
 local g3d = require "g3d"
-local earth = g3d.newModel("g3dAssets/sphere.obj", "g3dAssets/earth.png", {0,0,4})
-local moon = g3d.newModel("g3dAssets/sphere.obj", "g3dAssets/moon.png", {5,0,4}, nil, {0.5,0.5,0.5})
+
+-- Object Creation
+-- local earth = g3d.newModel("g3dAssets/sphere.obj", "g3dAssets/earth.png", {0,0,4})
+-- local moon = g3d.newModel("g3dAssets/sphere.obj", "g3dAssets/moon.png", {-5,0,4}, nil, {0.5,0.5,0.5})
 local background = g3d.newModel("g3dAssets/sphere.obj", "g3dAssets/starfield.png", {0,0,0}, nil, {500,500,500})
+local grabableBall = g3d.newModel("g3dAssets/sphere.obj", "kenney_prototype_textures/red/texture_08.png", {10,0,4}, nil, {0.5,0.5,0.5})
+local bounds = g3d.newModel("custom_assets/plinkoBounds.obj", "kenney_prototype_textures/dark/texture_03.png", {10,0,4}, {math.pi/2,0,0}, {1,10,10})
 local timer = 0
 
 function love.mousemoved(x,y, dx,dy)
-    g3d.camera.firstPersonLook(dx,dy)
+    -- g3d.camera.firstPersonLook(dx,dy)
 end
 
 function love.update(dt)
     timer = timer + dt
-    moon:setTranslation(math.cos(timer)*5, 0, math.sin(timer)*5 +4)
-    moon:setRotation(0, math.pi - timer, 0)
-    g3d.camera.firstPersonMovement(dt)
+    -- moon:setTranslation(math.cos(timer)*5, 0, math.sin(timer)*5 +4)
+    -- moon:setRotation(0, math.pi - timer, 0)
+    -- g3d.camera.firstPersonMovement(dt)
     if love.keyboard.isDown("escape") then love.event.push("quit") end
 end
 
 function love.draw()
-    earth:draw()
-    moon:draw()
+    -- earth:draw()
+    -- moon:draw()
+	
     background:draw()
+	grabableBall:draw()
+	bounds:draw()
 end
